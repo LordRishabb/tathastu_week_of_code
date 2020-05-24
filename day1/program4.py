50783398136ca4516a11e8a43521d29c3821ba65
Mind Flayer <58518134+LordRishabb@users.noreply.github.com>
2020-05-24 23:32:43 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,7 @@
+costprice=int(input("Enter cost price:"))
+sellprice=int(input("Enter selling price:"))
+profit=sellprice-costprice
+newsp=1.05*profit+costprice
+print("Profit=",profit)
+print("New Selling price for increasing profit by 5%=",newsp)
+               
